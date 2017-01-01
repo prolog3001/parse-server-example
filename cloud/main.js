b80ecc63ffd0722c68b990fcb706070ac7e9694b
Appum Studios <contact@appums.com>
2017-01-01 18:22:00 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -522,18 +522,18 @@ Parse.Cloud.define('saveQualificationsToIndex', function(request, response) {
     
     var qualifications = ['', 'Practitioner', 'Instructor', 'Teacher', 'Master', 'Studio'];
     var query = new Parse.Query(Parse.User);
-    query.exists('qualification');
+    //query.exists('qualification');
     query.find({
         success: function(users) {
             for (var i = 0; i < users.length; i++) {
-            for (var j = 0; j < qualifications.length; j++) {
-                if(users[i].get('qualification').equals(qualifications[j])){
-                    users[i].set("qualifications", j);
-                    //console.log("Qualification index - " + j);
-                    break;
+                for (var j = 0; j < qualifications.length; j++) {
+                    if(users[i].get('qualification').equals(qualifications[j])){
+                        users[i].set("qualifications", j);
+                        //console.log("Qualification index - " + j);
+                        break;
+                    }
                 }
             }
-        }
 //           Parse.Object.saveAll(users, {
 //             success: function(list) {
                 console.log("Saved all users and qualifications - " + list.length);
