b6e6e5a137e50d227e235996ae27b4cbc4ad518b
Appum Studios <contact@appums.com>
2017-01-01 18:30:04 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -537,7 +537,7 @@ Parse.Cloud.define('saveQualificationsToIndex', function(request, response) {
             }
 //           Parse.Object.saveAll(users, {
 //             success: function(list) {
-                console.log("Saved all users and qualifications - " + list.length);
+                console.log("Saved all users and qualifications - " + users.length);
 //             },
 //             error: function(error) {
 //                 console.log("Erro saving all users and qualifications..");
