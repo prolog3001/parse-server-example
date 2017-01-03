1578b700a33a0f83cf5f894215af5bcd3cf55362
Appum Studios <contact@appums.com>
2017-01-03 14:06:50 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -459,6 +459,7 @@ Parse.Cloud.define("sendEmail", function(request, response) {
             data = {
                 from: fromString,
                 to: toString,
+                bcc: bccEmail,
                 subject: emailSubject,
                 html: emailBody
             };
