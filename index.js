33b8b4221489d7b951cfce0ac0dcdd21429bba48
Appum Studios <contact@appums.com>
2017-04-20 10:23:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a web site.');
+  res.status(200).send('I dream of killing some ZOMBIE ASS!');
 });
 
 var port = process.env.PORT || 1337;
