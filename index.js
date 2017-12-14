5541797de62f4af024468db1e23ae58cd664cfb2
MedidateSandbox <33726864+MedidateSandbox@users.noreply.github.com>
2017-12-14 14:02:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -55,8 +55,30 @@ var api = new ParseServer({
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
+//-----------------REAL CODE-------------------//
+// var app = express();
 
+// // Serve the Parse API on the /parse URL prefix
+// var mountPath = process.env.PARSE_MOUNT || '/parse';
+// app.use(mountPath, api);
+
+// // Parse Server plays nicely with the rest of your web routes
+// app.get('/', function(req, res) {
+//   res.status(200).send('I dream of killing some ZOMBIE ASS!');
+// });
+
+// var port = process.env.PORT || 1337;
+// app.listen(port, function() {
+//     console.log('parse-server-example running on port ' + port + '.');
+// });
+
+//-----------------REAL CODE-------------------//
+
+//-----------------TEST CODE-------------------//
 var app = express();
+
+// Serve static assets from the /public folder
+app.use('/public', express.static(path.join(__dirname, '/public')));
 
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
@@ -64,10 +86,21 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('I dream of killing some ZOMBIE ASS!');
+  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+});
+
+// There will be a test page available on the /test path of your server url
+// Remove this before launching your app
+app.get('/test', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/test.html'));
 });
 
 var port = process.env.PORT || 1337;
-app.listen(port, function() {
+var httpServer = require('http').createServer(app);
+httpServer.listen(port, function() {
     console.log('parse-server-example running on port ' + port + '.');
 });
+
+// This will enable the Live Query real-time server
+ParseServer.createLiveQueryServer(httpServer);
+//-----------------TEST CODE-------------------//
