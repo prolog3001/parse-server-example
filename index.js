4c331739b197b4000f1a21d51258186c1bc15322
digidine-tables <34857386+digidine-tables@users.noreply.github.com>
2017-12-26 12:27:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// Example express application adding the parse-server module to expose Parse
-// compatible API routes.
-// import OneSignalPushAdapter from __dirname + '/adapters/OneSignalPushAdapter';
-
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 
@@ -10,13 +6,6 @@ var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI;
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
-
-//Mailgun - reset password
-// var simpleMailgunAdapter = require('parse-server/lib/Adapters/Email/SimpleMailgunAdapter')({
-//   apiKey: process.env.MAILGUN_KEY || '',
-//   domain: process.env.DOMAIN || 'medidatewith.me',
-//   fromAddress: process.env.MAILGUN_FROM_ADDRESS || 'no-reply@medidatewith.me'
-// });
 
 //Push Adapter
 var OneSignalPushAdapter = require('parse-server-onesignal-push-adapter');
@@ -28,21 +17,21 @@ var oneSignalPushAdapter = new OneSignalPushAdapter({
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  appName: 'Medidate Sandbox',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || process.env.ALT_SERVER_URL,  // Don't forget to change to https if needed
-  publicServerURL: process.env.PUBLIC_SERVER_URL || process.env.ALT_PUBLIC_SERVER_URL,
+  appId: process.env.APP_ID,
+  appName: 'DigiDine Table Planner',
+  masterKey: process.env.MASTER_KEY,
+  serverURL: process.env.SERVER_URL,  // Don't forget to change to https if needed
+  publicServerURL: process.env.PUBLIC_SERVER_URL,
   liveQuery: {
-    classNames: ["Message"] // List of classes to support for query subscriptions
+    classNames: ["User"] // List of classes to support for query subscriptions
   },
   verifyUserEmails: true,
   emailAdapter: {
       module: 'parse-server-simple-mailgun-adapter',
       options: {
-      fromAddress: process.env.MAILGUN_FROM_ADDRESS || 'no-reply@medidatewith.me',
-      apiKey: process.env.MAILGUN_KEY || '',
-      domain: process.env.DOMAIN || 'medidatewith.me',
+      fromAddress: process.env.MAILGUN_FROM_ADDRESS,
+      apiKey: process.env.MAILGUN_KEY,
+      domain: process.env.DOMAIN
       }
       },
   push: {
@@ -50,7 +39,6 @@ var api = new ParseServer({
   }
 });
 
-//-----------------REAL CODE-------------------//
 var app = express();
 
 // Serve the Parse API on the /parse URL prefix
@@ -67,11 +55,6 @@ var httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
     console.log('parse-server-example running on port ' + port + '.');
 });
-// app.listen(port, function() {
-//     console.log('parse-server-example running on port ' + port + '.');
-// });
 
 // This will enable the Live Query real-time server
 ParseServer.createLiveQueryServer(httpServer);
-
-//-----------------REAL CODE-------------------//
