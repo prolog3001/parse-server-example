7be8a55f6ed37326f38ec975604c7063fc0e5043
Mat <34857386+matan-d@users.noreply.github.com>
2018-07-30 10:59:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,60 @@
-// Example express application adding the parse-server module to expose Parse
-// compatible API routes.
-
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
-var path = require('path');
+var OneSignalPushAdapter = require('parse-server-onesignal-push-adapter');
+var S3Adapter = require('parse-server-s3-adapter');
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI;
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
+//Push Adapter
+// var oneSignalPushAdapter = new OneSignalPushAdapter({
+//   oneSignalAppId:process.env.ONE_SIGNAL_APP_ID,
+//   oneSignalApiKey:process.env.ONE_SIGNAL_REST_API_KEY
+// });
+
+//Files Adapter
+var s3Adapter = new S3Adapter(
+  process.env.S3_ACCESS_KEY, 
+  process.env.S3_SECRET_KEY, 
+  process.env.S3_BUCKET, {
+  region: 'us-east-1',
+  bucketPrefix: '',
+  directAccess: false,
+  baseUrl: '',
+  signatureVersion: 'v4',
+  globalCacheControl: 'public, max-age=86400'  // 24 hrs Cache-Control.
+});
+
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  cloud: __dirname + '/cloud/main.js',
+  appId: process.env.APP_ID,
+  appName: 'DigiDine Table Planner',
+  masterKey: process.env.MASTER_KEY,
+  serverURL: process.env.SERVER_URL,  // Don't forget to change to https if needed
+  publicServerURL: process.env.PUBLIC_SERVER_URL,
+  filesAdapter: s3Adapter,
   liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+    classNames: ["User", "Business", "Order", 
+                 "RestaurantOrderSummary", "Table", "UserWaiterRelation"] // List of classes to support for query subscriptions
+  },
+  emailAdapter: {
+      module: 'parse-server-simple-mailgun-adapter',
+      options: {
+      fromAddress: process.env.MAILGUN_FROM_ADDRESS,
+      apiKey: process.env.MAILGUN_KEY,
+      domain: process.env.DOMAIN
+      }
+      },
+//   push: {
+//      adapter: oneSignalPushAdapter
+//   }
 });
-// Client-keys like the javascript key or the .NET key are not necessary with parse-server
-// If you wish you require them, you can set them as options in the initialization above:
-// javascriptKey, restAPIKey, dotNetKey, clientKey
 
 var app = express();
-
-// Serve static assets from the /public folder
-app.use('/public', express.static(path.join(__dirname, '/public')));
 
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
@@ -36,13 +62,7 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
-});
-
-// There will be a test page available on the /test path of your server url
-// Remove this before launching your app
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
+  res.status(200).send('I dream of planning tables!');
 });
 
 var port = process.env.PORT || 1337;
