92d8e68fb66db8d68869d7077df97a32a74b9c4e
mat d <email@example.com>
2019-08-08 12:12:49 +0300

    after save

--- cloud/main.js
+++ cloud/main.js
@@ -46,27 +46,44 @@ Parse.Cloud.define("closeOpenedOrders", background.closeOpenedOrders);
 Parse.Cloud.job("closeOpenedOrders", background.closeOpenedOrders);
 
 Parse.Cloud.afterSave("RestaurantOrderSummary", function (request) {
-    var orderSummaryObject = request.object;
-    console.log("Object Type", orderSummaryObject.className);
-    console.log("Object Table", orderSummaryObject.get("table"));
-    if (orderSummaryObject.className == "RestaurantOrderSummary" &&
-        orderSummaryObject.get("table") &&
-        (!orderSummaryObject.get("table").id || orderSummaryObject.get("table").get("title") == "TA")) {
+    var orderSummaryPointer = request.object;
+    console.log("Object Type", orderSummaryPointer.className);
+    console.log("Object Table", orderSummaryPointer.get("table"));
 
-        orderSummaryObject.unset("table");
-        if (orderSummaryObject.get("table")) {
-            console.log("Fail, cant delete order table");
-            return;
+    var userQuery = new Parse.Query("RestaurantOrderSummary");
+    userQuery.containedIn("objectId", orderSummaryPointer.id);
+    userQuery.include("table");
+    
+    userQuery.find({
+      useMasterKey: true, //This is for the new version
+      success: function(orderSummaries) {
+        console.log("Found..." + orderSummaries.length);
+        orderSummaryObject = orderSummaries[0];
+
+        if (orderSummaryObject && orderSummaryObject.className == "RestaurantOrderSummary" &&
+            orderSummaryObject.get("table") &&
+            (!orderSummaryObject.get("table").id || orderSummaryObject.get("table").get("title") == "TA")) {
+    
+            orderSummaryObject.unset("table");
+            if (orderSummaryObject.get("table")) {
+                console.log("Fail, cant delete order table");
+                return;
+            }
+    
+            orderSummaryObject.save(null, { useMasterKey: true })
+                .then(function (result) {
+                    console.log("Success saving after table removal", result);
+                },
+                    function (error) {
+                        console.log("Error", error);
+                    });
+        } else {
+            console.log("Not OrderSummary or dont need changes");
         }
-
-        orderSummaryObject.save(null, { useMasterKey: true })
-            .then(function (result) {
-                console.log("Success saving after table removal", result);
-            },
-                function (error) {
-                    console.log("Error", error);
-                });
-    } else {
-        console.log("Not OrderSummary or dont need changes");
-    }
+      },
+  
+      error: function(error) {
+        console.log("Query Error", error);
+      }
+    });
 })
