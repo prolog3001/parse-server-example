126b1ec85b04ce04f0fb74013f2ed2e3f1a7d341
mat d <email@example.com>
2019-08-08 12:18:15 +0300

    after save

--- cloud/main.js
+++ cloud/main.js
@@ -53,37 +53,62 @@ Parse.Cloud.afterSave("RestaurantOrderSummary", function (request) {
     var userQuery = new Parse.Query("RestaurantOrderSummary");
     userQuery.containedIn("objectId", orderSummaryPointer.id);
     userQuery.include("table");
-    
-    userQuery.find({
-      useMasterKey: true, //This is for the new version
-      success: function(orderSummaries) {
-        console.log("Found..." + orderSummaries.length);
-        orderSummaryObject = orderSummaries[0];
 
-        if (orderSummaryObject && orderSummaryObject.className == "RestaurantOrderSummary" &&
-            orderSummaryObject.get("table") &&
-            (!orderSummaryObject.get("table").id || orderSummaryObject.get("table").get("title") == "TA")) {
-    
-            orderSummaryObject.unset("table");
-            if (orderSummaryObject.get("table")) {
-                console.log("Fail, cant delete order table");
-                return;
+    userQuery.find({
+        useMasterKey: true, //This is for the new version
+        success: function (orderSummaries) {
+            console.log("Found..." + orderSummaries.length);
+            orderSummaryObject = orderSummaries[0];
+
+            if (orderSummaryObject && orderSummaryObject.className == "RestaurantOrderSummary" &&
+                orderSummaryObject.get("table") &&
+                (!orderSummaryObject.get("table").id || orderSummaryObject.get("table").get("title") == "TA")) {
+
+                orderSummaryObject.unset("table");
+                if (orderSummaryObject.get("table")) {
+                    console.log("Fail, cant delete order table");
+                    return;
+                }
+
+                orderSummaryObject.save(null, { useMasterKey: true })
+                    .then(function (result) {
+                        console.log("Success saving after table removal", result);
+                    },
+                        function (error) {
+                            console.log("Error", error);
+                        });
+            } else {
+                console.log("Not OrderSummary or dont need changes");
             }
-    
-            orderSummaryObject.save(null, { useMasterKey: true })
-                .then(function (result) {
-                    console.log("Success saving after table removal", result);
-                },
-                    function (error) {
-                        console.log("Error", error);
-                    });
-        } else {
-            console.log("Not OrderSummary or dont need changes");
+        },
+
+        error: function (error) {
+            console.log("Query Error", error);
         }
-      },
-  
-      error: function(error) {
-        console.log("Query Error", error);
-      }
     });
+})
+
+Parse.Cloud.afterSave("Table", function (request) {
+    if (request.object.existed() === false) {
+        // It's a new object 
+        var table = request.object;
+        console.log("Object Type", table.className);
+        console.log("Object Title", table.get("title"));
+
+        if (table && table.className == "Table" &&
+            table.get("title") == "TA") {
+            table.destroy({
+                success: function(res) {
+                    log('success destroy', res)
+                },
+                error: function(error) {
+                    log('Failed to destroy object, with error code: ' + error.message);
+                }
+            });
+        } else {
+            console.log("Not Table or dont need changes");
+        }
+    } else {
+        // It's an existing object
+    }
 })
