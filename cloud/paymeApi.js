2d188a290c583a05caa3b264f8c44ffa81880bdd
contact@appums.com <AppumStudioGitHubPass432>
2020-07-12 11:47:19 +0300

    sellerPaidDirectly

--- cloud/paymeApi.js
+++ cloud/paymeApi.js
@@ -19,7 +19,7 @@ module.exports = {
 async function purchaseProduct(request, response) {
     console.log('purchaseProduct');
     console.log('params', request.params);
-    let { businessId, productType, clientId, productId, amount, tip} = request.params;
+    let { businessId, productType, clientId, productId, amount, tip } = request.params;
 
     let client = await utils.getObjectById('User', clientId);
     var product;
@@ -57,9 +57,9 @@ async function purchaseProduct(request, response) {
             market_fee: parseFloat(MERCHANT_FEE),
         }),
 
-        currency: business.get('currency').equalsIgnoreCase("₪") ? "NIS" : "USD",
+        currency: business.get('currency').equals("₪") ? "NIS" : "USD",
         product_name: product ? product.id : (locale == 'he' ? 'סכום חופשי' : 'Free Amount'),
-        sale_callback_url: process.env.WEBHOOK_BASE_URL + '/api/payment-request/success' + getWebhookUrl({ productType, product, business, client, amount, tip}),
+        sale_callback_url: process.env.WEBHOOK_BASE_URL + '/api/payment-request/success' + getWebhookUrl({ productType, product, business, client, amount, tip }),
         sale_name: (locale == 'he' ? 'תשלום על הזמנה על ידי: ' : 'Payment for Order by: ') + client.get('name'),
 
         layout: 'micro_ltr',
@@ -112,7 +112,7 @@ async function purchaseProduct(request, response) {
 
 function getWebhookUrl(params) {
     console.log('getWebhookUrl params', params);
-    let { productType, product, business, client, amount, tip} = params;
+    let { productType, product, business, client, amount, tip } = params;
 
     var webhookParams = '?businessId=' + business.id + '&buyerId=' + client.id + '&productType=' + productType + '&tip=' + tip;
 
@@ -134,24 +134,24 @@ function getWebhookUrl(params) {
 
 function sellerPaidDirectly(seller) {
     return new Promise(async (resolve, reject) => {
-        if(!process.env.PAYME_KEY || process.env.PAYME_KEY.length == 0){
+        if (process.env.PAYME_KEY.length > 0) {
+            let result = await axios({
+                method: 'post',
+                url: process.env.PAYME_URL + '/api/get-sellers',
+                data: { payme_client_key: process.env.PAYME_KEY, seller_payme_id: seller.get('payme_seller_id') },
+                headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+                maxContentLength: Infinity,
+                maxBodyLength: Infinity
+            }).catch(error => {
+                console.log(error);
+            })
+
+            let isPaidDirectly = (result && result.data && result.data.items && result.data.items.length > 0 && result.data.items[0].seller_is_paid_directly);
+            console.log('isPaidDirectly', isPaidDirectly);
+            resolve(isPaidDirectly);
+        } else {
             resolve(false);
         }
-        
-        let result = await axios({
-            method: 'post',
-            url: process.env.PAYME_URL + '/api/get-sellers',
-            data: { payme_client_key: process.env.PAYME_KEY, seller_payme_id: seller.get('payme_seller_id') },
-            headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-            maxContentLength: Infinity,
-            maxBodyLength: Infinity
-        }).catch(error => {
-            console.log(error);
-        })
-
-        let isPaidDirectly = (result && result.data && result.data.items && result.data.items.length > 0 && result.data.items[0].seller_is_paid_directly);
-        console.log('isPaidDirectly', isPaidDirectly);
-        resolve(isPaidDirectly);
     });
 }
 
@@ -159,89 +159,89 @@ function sellerPaidDirectly(seller) {
 async function refundProduct(request, response) {
     console.log('refundProduct');
     console.log('params', request.params);
-    
-    if(!process.env.PAYME_KEY || process.env.PAYME_KEY.length == 0){
-        response.error('Trouble refunding this payment');
-    }
 
-    let { businessId, productType, clientId, productId } = request.params;
+    if (process.env.PAYME_KEY.length > 0) {
+        let { businessId, productType, clientId, productId } = request.params;
 
-    let client = await utils.getObjectById('User', clientId);
-    var product;
-    var business;
+        let client = await utils.getObjectById('User', clientId);
+        var product;
+        var business;
 
-    switch (parseInt(productType)) {
-        case 0://Free AMount
-            business = await utils.getObjectById('Business', businessId);
-            break;
-        case 1://Order
-            product = await utils.getObjectById('RestaurantOrderSummary', productId);
-            business = await utils.getObjectById('Business', businessId);
-            break;
-        default:
-            break;
-    }
+        switch (parseInt(productType)) {
+            case 0://Free AMount
+                business = await utils.getObjectById('Business', businessId);
+                break;
+            case 1://Order
+                product = await utils.getObjectById('RestaurantOrderSummary', productId);
+                business = await utils.getObjectById('Business', businessId);
+                break;
+            default:
+                break;
+        }
 
-    let { payment} = await getPaymentObject({ productType, product, client});
+        let { payment } = await getPaymentObject({ productType, product, client });
 
-    let canRefund = await getSaleStatus(payment);
+        let canRefund = await getSaleStatus(payment);
 
-    if (!canRefund) {
-        response.error('Payment already refunded');
-        return;
-    }
+        if (!canRefund) {
+            response.error('Payment already refunded');
+            return;
+        }
 
-    console.log('payment', payment);
+        console.log('payment', payment);
 
-    var locale = business.get('language');
-    locale = locale ? (locale.includes('he') ? 'he' : 'en') : 'en';
+        var locale = business.get('language');
+        locale = locale ? (locale.includes('he') ? 'he' : 'en') : 'en';
 
-    var params = {
-        payme_client_key: process.env.PAYME_KEY,
-        seller_payme_id: business.get('payme_seller_id'),
-        payme_sale_id: payment.get('payme_sale_id'),
-        language: locale
-    };
+        var params = {
+            payme_client_key: process.env.PAYME_KEY,
+            seller_payme_id: business.get('payme_seller_id'),
+            payme_sale_id: payment.get('payme_sale_id'),
+            language: locale
+        };
 
-    let refundUrl = process.env.PAYME_URL + '/api/refund-sale';
+        let refundUrl = process.env.PAYME_URL + '/api/refund-sale';
 
-    console.log('params', params);
-    console.log('refundUrl', refundUrl);
+        console.log('params', params);
+        console.log('refundUrl', refundUrl);
 
-    let result = await axios({
-        method: 'post',
-        url: refundUrl,
-        data: params,
-        headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-        maxContentLength: Infinity,
-        maxBodyLength: Infinity
-    }).catch(error => {
-        console.log(error);
-    })
+        let result = await axios({
+            method: 'post',
+            url: refundUrl,
+            data: params,
+            headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+            maxContentLength: Infinity,
+            maxBodyLength: Infinity
+        }).catch(error => {
+            console.log(error);
+        })
 
-    if ((result && result.data) && (result.data.status_code == 0 || result.data.payme_status === 'success')) {
-        console.log('result', result);
-        console.log('payme_status', result.data.payme_status);
-        response.success(result.data.sale_status);
-    } else if (result && result.data) {
-        console.log('status_error_code', result.data.status_error_code);
-        console.log('status_error_details', result.data.status_error_details);
-        response.error(result.data.status_error_details);
+        if ((result && result.data) && (result.data.status_code == 0 || result.data.payme_status === 'success')) {
+            console.log('result', result);
+            console.log('payme_status', result.data.payme_status);
+            response.success(result.data.sale_status);
+        } else if (result && result.data) {
+            console.log('status_error_code', result.data.status_error_code);
+            console.log('status_error_details', result.data.status_error_details);
+            response.error(result.data.status_error_details);
+        } else {
+            console.log('No result');
+            response.error('Trouble refunding this payment');
+        }
     } else {
-        console.log('No result');
         response.error('Trouble refunding this payment');
     }
 }
 
 function getPaymentObject(params) {
-    let { productType, product, client} = params;
+    let { productType, product, client } = params;
     return new Promise(async (resolve, reject) => {
 
         var query;
         switch (parseInt(productType)) {
             case 1://Order
                 var payment = await utils.getObjectById(PAYMENT_CLASS_NAME, paymentId);
-                resolve({payment: payment});
+                resolve({ payment: payment });
                 break;
             default:
                 break;
@@ -250,31 +250,30 @@ function getPaymentObject(params) {
 }
 
 function getSaleStatus(payment) {
-    if(!process.env.PAYME_KEY || process.env.PAYME_KEY.length == 0){
-        console.log('canRefund', false);
-        resolve(false);
-    }
-
     return new Promise(async (resolve, reject) => {
-        let params = { payme_client_key: process.env.PAYME_KEY, sale_payme_id: payment.get('payme_sale_id') };
-        let getSalesUrl = process.env.PAYME_URL + '/api/get-sales';
-        console.log('params', params);
-        console.log('getSalesUrl', getSalesUrl);
-        let result = await axios({
-            method: 'post',
-            url: getSalesUrl,
-            data: params,
-            headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-            maxContentLength: Infinity,
-            maxBodyLength: Infinity
-        }).catch(error => {
-            console.log(error);
-        })
-        console.log('getSaleStatus result', result);
-        let sale = (result && result.data && result.data.items && result.data.items.length > 0 && result.data.items[0]);
-        console.log('sale', sale ? JSON.stringify(sale) : 'Empty');
-        let canRefundSale = (result && result.data && result.data.items && result.data.items.length > 0 && result.data.items[0].sale_status !== 'refunded');
-        console.log('canRefund', canRefundSale);
-        resolve(canRefundSale);
+        if (process.env.PAYME_KEY.length > 0) {
+            let params = { payme_client_key: process.env.PAYME_KEY, sale_payme_id: payment.get('payme_sale_id') };
+            let getSalesUrl = process.env.PAYME_URL + '/api/get-sales';
+            console.log('params', params);
+            console.log('getSalesUrl', getSalesUrl);
+            let result = await axios({
+                method: 'post',
+                url: getSalesUrl,
+                data: params,
+                headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+                maxContentLength: Infinity,
+                maxBodyLength: Infinity
+            }).catch(error => {
+                console.log(error);
+            })
+            console.log('getSaleStatus result', result);
+            let sale = (result && result.data && result.data.items && result.data.items.length > 0 && result.data.items[0]);
+            console.log('sale', sale ? JSON.stringify(sale) : 'Empty');
+            let canRefundSale = (result && result.data && result.data.items && result.data.items.length > 0 && result.data.items[0].sale_status !== 'refunded');
+            console.log('canRefund', canRefundSale);
+            resolve(canRefundSale);
+        } else {
+            resolve(false);
+        }
     });
 }
