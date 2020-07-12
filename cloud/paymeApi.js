ee2e4281daffd741540519ea9290900de3e1338e
contact@appums.com <AppumStudioGitHubPass432>
2020-07-12 11:51:19 +0300

    currency

--- cloud/paymeApi.js
+++ cloud/paymeApi.js
@@ -46,6 +46,9 @@ async function purchaseProduct(request, response) {
 
     let buyerKey = client.get('payme_buyer_key');
     let isBuyerKeyValid = !utils.isEmpty(buyerKey);
+    let currency = business.get('currency');
+    currency = currency == '₪' ? "NIS" : "USD";
+
     console.log('buyerKey', buyerKey);
     console.log('amountToPay', amountToPay);
 
@@ -57,7 +60,7 @@ async function purchaseProduct(request, response) {
             market_fee: parseFloat(MERCHANT_FEE),
         }),
 
-        currency: business.get('currency').equals("₪") ? "NIS" : "USD",
+        currency: ,
         product_name: product ? product.id : (locale == 'he' ? 'סכום חופשי' : 'Free Amount'),
         sale_callback_url: process.env.WEBHOOK_BASE_URL + '/api/payment-request/success' + getWebhookUrl({ productType, product, business, client, amount, tip }),
         sale_name: (locale == 'he' ? 'תשלום על הזמנה על ידי: ' : 'Payment for Order by: ') + client.get('name'),
