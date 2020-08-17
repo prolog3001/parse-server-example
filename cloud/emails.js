b28298e19ae082b0bd18e267907ebdf705c19f58
contact@appums.com <AppumStudioGitHubPass432>
2020-08-17 11:43:20 +0300

    sendNewsletter

--- cloud/emails.js
+++ cloud/emails.js
@@ -56,61 +56,90 @@ async function sendTestEmail(request, response) {
 
 async function sendNewsletter(request, response) {
   try {
-    var params = {};
+    var fs = require('fs');
+    var emailBody = fs.readFileSync('cloud/HTML/User Actions/email_newsletter.html', "utf-8");
+
+    var userQuery = new Parse.Query(Parse.User);
+    userQuery.exists("email");
+    userQuery.notEqualTo("blocked", true);
+
+    //DEBUG ONLY
+    userQuery.equalTo("email", process.env.MAILGUN_TEST_EMAIL);
+
+    userQuery.limit(1000);
+    userQuery.find({
+      useMasterKey: true,
+      success: function (users) {
+        console.log("Found..." + users.length);
+        sendBulkEmail("Test Newsletter", emailBody, users);
+        if (response)
+          response.success("Newsletter Sent");
+        else
+          return;
+      },
+
+      error: function (error) {
+        if (response)
+          response.error(error);
+        else
+          return error;
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    if (response)
+      response.error(error);
+    else
+      return error;
+  }
+}
+
+async function sendBulkEmail(emailSubject, emailBody, users) {
+  try {
+    console.log("sendBulkEmail..." + users.length);
+    console.log("sendBulkEmail..." + emailBody);
 
     var fromEmail = "info@dreamdiner.io";
     var fromName = "DreamDiner";
     var fromString = fromName + " <" + fromEmail + ">";
-
-    var toString = "DreamDiner Test" + " <" + process.env.MAILGUN_TEST_EMAIL + ">"
-
-    var emailSubject = "Welcome to DreamDiner";
-
-    var fs = require('fs');
-    var emailBody = fs.readFileSync('cloud/HTML/User Actions/email_welcome.html', "utf-8");
-    emailBody = utils.replaceAll(emailBody, "admin_name", "DreamDiner Test");
-
-    var data = {
-      from: fromString,
-      to: toString,
-      subject: emailSubject,
-      html: emailBody
-    };
 
     var simpleMailgunAdapter = require('mailgun-js')({
       apiKey: process.env.MAILGUN_KEY || '',
       domain: process.env.MAILGUN_DOMAIN
     });
 
-    const recipients = ['email1@gmail.com', 'email2@gmail.com'];
-    const recipientVars = {
-      'email1@gmail.com': {
-        id: 1,
-        subject: 'Subject 1',
-        name: 'Name 1'
-      },
-      'email2@gmail.com': {
-        id: 2,
-        subject: 'Subject 2',
-        name: 'Name 2'
-      }
-    };
+    var recipients = [];
+    var recipientVars = {};
 
-    const envelope = {
-      from: 'Sender <sender@gmail.com>',
+    for (var i = 0; i < users.length; i++) {
+      var user = users[i];
+
+      var recepient = user.get("email");
+      var recepientVar = {
+        id: user.id,
+        subject: emailSubject,
+        name: user.get("name")
+      };
+
+      recipients.push(recepient);
+      recipientVars[recepient] = recepientVar;
+    }
+
+    var envelope = {
+      from: fromString,
       to: recipients,
       subject: '%recipient.subject%',
-      html: 'Hey <strong>%recipient.name%<strong>',
+      html: emailBody,
       'recipient-variables': recipientVars,
     };
 
     simpleMailgunAdapter.messages().send(envelope, function (error, body) {
       if (error) {
         console.log("got an error in sendBulkEmail: " + error);
-        return;
+        return error;
       } else {
-        console.log("bulk email sent to " + process.env.MAILGUN_TEST_EMAIL);
-        return;
+        console.log("bulk email sent");
+        return "bulk email sent";
       }
     });
   } catch (error) {
