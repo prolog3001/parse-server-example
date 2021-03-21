6274ab0600043a139eb4d4de5b3415056cde0336
contact@appums.com <AppumStudioGitHubPass432>
2021-03-21 09:03:15 +0200

    sendTestEmail

--- cloud/emails.js
+++ cloud/emails.js
@@ -17,50 +17,33 @@ module.exports = {
 async function sendTestEmail(request, response) {
   try {
     var params = {};
+    var fromEmail = "info@dreamdiner.io";
+    var fromName = "DreamDiner";
+    var fromString = fromName + " <" + fromEmail + ">";
+
+    var toString = "DreamDiner Test" + " <" + process.env.MAILGUN_TEST_EMAIL + ">"
+
+    var emailSubject = "Welcome to DreamDiner";
+
+    var fs = require('fs');
+    var emailBody = fs.readFileSync('cloud/HTML/User Actions/email_welcome.html', "utf-8");
+    emailBody = utils.replaceAll(emailBody, "admin_name", "DreamDiner Test");
+
+    var data = {
+      from: fromString,
+      to: toString,
+      subject: emailSubject,
+      html: emailBody
+    };
+
     const sgMail = require('@sendgrid/mail')
     sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-    const msg = {
-      to: 'matandahan@gmail.com',
-      from: 'info@dreamdiner.io',
-      subject: 'Sending with SendGrid is Fun',
-      text: 'and easy to do anywhere, even with Node.js',
-      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    }
-    sgMail
-      .send(msg)
+    sgMail.send(data)
       .then(() => {
         console.log('Email sent')
-      })
-      .catch((error) => {
+      }).catch((error) => {
         console.error(error)
       })
-    // var fromEmail = "info@dreamdiner.io";
-    // var fromName = "DreamDiner";
-    // var fromString = fromName + " <" + fromEmail + ">";
-
-    // var toString = "DreamDiner Test" + " <" + process.env.MAILGUN_TEST_EMAIL + ">"
-
-    // var emailSubject = "Welcome to DreamDiner";
-
-    // var fs = require('fs');
-    // var emailBody = fs.readFileSync('cloud/HTML/User Actions/email_welcome.html', "utf-8");
-    // emailBody = utils.replaceAll(emailBody, "admin_name", "DreamDiner Test");
-
-    // var data = {
-    //   from: fromString,
-    //   to: toString,
-    //   subject: emailSubject,
-    //   html: emailBody
-    // };
-
-    // const sgMail = require('@sendgrid/mail')
-    // sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-    // sgMail.send(data)
-    //   .then(() => {
-    //     console.log('Email sent')
-    //   }).catch((error) => {
-    //     console.error(error)
-    //   })
   } catch (error) {
     console.log(error);
     return error;
